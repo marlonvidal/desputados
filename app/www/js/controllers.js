b06c709e7e91538cb9426fbf9c44d2fad81c5cb4
wandsv <vieira.wanderson@gmail.com>
2016-05-27 03:31:41 -0300

    First commit

---
+++ app/www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('menuCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_BioCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_QuimCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_FisCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_PortCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_HisCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_GeoCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_InglesCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_AtualCtrl', function($scope) {
+
+})
+   
+.controller('pergunta_FilSolCtrl', function($scope) {
+
+})
+   
+.controller('rankCtrl', function($scope) {
+
+})
+   
+.controller('resultadoCtrl', function($scope) {
+
+})
+ 
